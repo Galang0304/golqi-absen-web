2f755472b2d68fbac127477053c5afb289b4a7da
Galang0304 <1058441117422@student.unismuh.ac.id>
2026-07-08 22:19:24 +0800

    feat: tunjangan pindah ke grup Keuangan & Laporan + tombol lihat password di login

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -52,7 +52,6 @@ const NAV: NavEntry[] = [
       items: [
         { name: 'Cabang / Outlet', href: '/dashboard/cabang', icon: icon('M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4') },
         { name: 'Shift Kerja', href: '/dashboard/shift', icon: icon('M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z') },
-        { name: 'Tunjangan', href: '/dashboard/tunjangan', icon: icon('M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z') },
       ],
     },
   },
@@ -62,6 +61,7 @@ const NAV: NavEntry[] = [
       name: 'Keuangan & Laporan',
       icon: icon('M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'),
       items: [
+        { name: 'Tunjangan', href: '/dashboard/tunjangan', icon: icon('M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z') },
         { name: 'Penggajian', href: '/dashboard/penggajian', icon: icon('M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z') },
         { name: 'Laporan Kehadiran', href: '/dashboard/laporan', icon: icon('M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z') },
       ],
